8a46117729d44aa32bd4b9df047cd772702e7bff
ELAYADE ISMAIL <elayadeismail@gmail.com>
2022-02-20 15:56:46 +0100

    add filtering to collection admin page

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,57 @@
 from django.contrib import admin
+from django.db.models import Count, QuerySet
+from django.http import HttpRequest
+from django.urls import reverse
+from django.utils.html import format_html, urlencode
 
 from . import models
 
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'unit_price',
+                    'inventory_status', 'collection_title']
+    list_editable = ['unit_price']
+    list_per_page = 10
+    list_select_related = ['collection']
+
+    def collection_title(self, product):
+        return product.collection.title
+
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return 'Low'
+        return 'OK'
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'placed_at', 'customer']
+
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership']
+    list_editable = ['membership']
+    list_per_page = 10
+
+
 # Register your models here.
-admin.site.register(models.Collection)
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'products_count']
+
+    @admin.display(ordering='products_count')
+    def products_count(self, collection):
+        url = (reverse('admin:store_product_changelist')
+               + "?"
+               + urlencode({
+                   'collection__id': str(collection.id)
+               }))
+        return format_html('<a href="{}">{}</a>', url, collection.products_count)
+
+    def get_queryset(self, request: HttpRequest) -> QuerySet:
+        return super().get_queryset(request).annotate(
+            products_count=Count('product')
+        )
