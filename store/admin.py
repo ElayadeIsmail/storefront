088f8f8c0e6f23bcdd1f9a9b5690b3b2d032f56a
ELAYADE ISMAIL <elayadeismail@gmail.com>
2022-02-20 16:15:13 +0100

    add new filter field to products

--- store/admin.py
+++ store/admin.py
@@ -7,6 +7,20 @@ from django.utils.html import format_html, urlencode
 from . import models
 
 
+class InventoryFilter(admin.SimpleListFilter):
+    title = 'inventory'
+    parameter_name = 'inventory'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('<10', 'Low')
+        ]
+
+    def queryset(self, request, queryset: QuerySet):
+        if self.value == '10':
+            return queryset.filter(inventory__lt=10)
+
+
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'unit_price',
@@ -14,6 +28,7 @@ class ProductAdmin(admin.ModelAdmin):
     list_editable = ['unit_price']
     list_per_page = 10
     list_select_related = ['collection']
+    list_filter = ['collection', 'last_update', InventoryFilter]
 
     def collection_title(self, product):
         return product.collection.title
@@ -32,9 +47,25 @@ class OrderAdmin(admin.ModelAdmin):
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'membership']
+    list_display = ['first_name', 'last_name', 'membership', 'orders_count']
     list_editable = ['membership']
     list_per_page = 10
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
+
+    @admin.display(ordering="orders_count")
+    def orders_count(self, customer):
+        url = (reverse('admin:store_order_changelist')
+               + '?'
+               + urlencode({
+                   'customer__id': str(customer.id)
+               })
+               )
+        return format_html('<a href="{}">{}</a>', url, customer.orders_count)
+
+    def get_queryset(self, request: HttpRequest) -> QuerySet:
+        return super().get_queryset(request).annotate(
+            orders_count=Count('order')
+        )
 
 
 # Register your models here.
