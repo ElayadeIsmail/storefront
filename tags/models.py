d00ef6a607bab6e89292925a378768057d6ff5b0
ELAYADE ISMAIL <elayadeismail@gmail.com>
2022-02-20 14:21:50 +0100

    register collection model for admin

--- tags/models.py
+++ tags/models.py
@@ -5,11 +5,24 @@ from django.db import models
 # Create your models here.
 
 
+class TaggetItemManager(models.Manager):
+    def get_tags_for(self, obj_type, obj_id):
+        content_type = ContentType.objects.get_for_model(obj_type)
+
+        return TaggedItem.objects \
+            .select_related('tag') \
+            .filter(
+                content_type=content_type,
+                object_id=obj_id
+            )
+
+
 class Tag(models.Model):
     label = models.CharField(max_length=255)
 
 
 class TaggedItem():
+    objects = TaggetItemManager()
     # What tag applied to what object
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
