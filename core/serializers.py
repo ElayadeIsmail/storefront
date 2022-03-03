e576d3c679306d0302e4b69ee586acaf5a59e1b6
ELAYADE ISMAIL <elayadeismail@gmail.com>
2022-03-03 16:35:37 +0100

    update current user serializer

--- core/serializers.py
+++ core/serializers.py
@@ -1,8 +1,13 @@
-from djoser.serializers import \
-    UserCreateSerializer as BaseUserCreateSerializer
+from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
+from djoser.serializers import UserSerializer as BaseUserSerializer
 
 
 class UserCreateSerializer(BaseUserCreateSerializer):
     class Meta(BaseUserCreateSerializer.Meta):
         fields = ['id', 'username', 'first_name',
                   'last_name', 'email', 'password']
+
+
+class UserSerializer(BaseUserSerializer):
+    class Meta(BaseUserSerializer.Meta):
+        fields = ['id', 'first_name', 'last_name', 'email', 'username']
--- storefront/settings.py
+++ storefront/settings.py
@@ -152,7 +152,8 @@ AUTH_USER_MODEL = 'core.User'
 
 DJOSER = {
     'SERIALIZERS': {
-        'user_create': 'core.serializers.UserCreateSerializer'
+        'user_create': 'core.serializers.UserCreateSerializer',
+        'current_user': 'core.serializers.UserSerializer'
     }
 }
 
