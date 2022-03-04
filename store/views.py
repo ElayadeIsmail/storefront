66f577ad2948572339da317f00979413bdfebfa0
ELAYADE ISMAIL <elayadeismail@gmail.com>
2022-03-04 10:22:33 +0100

    disable converting decimal to strings

--- store/views.py
+++ store/views.py
@@ -9,7 +9,7 @@ from rest_framework.decorators import action
 from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.mixins import (CreateModelMixin, DestroyModelMixin,
                                    RetrieveModelMixin, UpdateModelMixin)
-from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet, ModelViewSet
 
