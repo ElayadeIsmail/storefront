5ba99579ac86817040cc9ca301e1947fbe963d64
ELAYADE ISMAIL <elayadeismail@gmail.com>
2022-03-06 12:14:39 +0100

    add authentication fixture

--- store/tests/conftest.py
+++ store/tests/conftest.py
@@ -1,7 +1,15 @@
 import pytest
+from django.contrib.auth.models import User
 from rest_framework.test import APIClient
 
 
 @pytest.fixture
 def api_client():
     return APIClient()
+
+
+@pytest.fixture
+def authenticate(api_client):
+    def do_authenticate(is_staff=False):
+        api_client.force_authenticate(user=User(is_staff=is_staff))
+    return do_authenticate
--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -1,6 +1,5 @@
 
 import pytest
-from django.contrib.auth.models import User
 from rest_framework import status
 
 
@@ -18,27 +17,25 @@ class TestCreateCollection:
         # AAA (Arrange,Act,Assert)
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
-    def test_if_user_is_not_admin_return_403(self, api_client, create_collection):
+    def test_if_user_is_not_admin_return_403(self, create_collection, authenticate):
         # AAA (Arrange,Act,Assert)
 
-        api_client.force_authenticate(user={})
+        authenticate()
         response = create_collection({'title': 'a'})
 
         assert response.status_code == status.HTTP_403_FORBIDDEN
 
-    def test_if_data_is_invalid_return_400(self, api_client, create_collection):
+    def test_if_data_is_invalid_return_400(self, create_collection, authenticate):
         # AAA (Arrange,Act,Assert)
-
-        api_client.force_authenticate(user=User(is_staff=True))
+        authenticate(is_staff=True)
         response = create_collection({'title': ''})
 
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert response.data['title'] is not None
 
-    def test_if_data_is_valid_return_201(self, api_client, create_collection):
+    def test_if_data_is_valid_return_201(self, create_collection, authenticate):
         # AAA (Arrange,Act,Assert)
-
-        api_client.force_authenticate(user=User(is_staff=True))
+        authenticate(is_staff=True)
         response = create_collection({'title': 'a'})
 
         assert response.status_code == status.HTTP_201_CREATED
